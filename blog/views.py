abd81aa0e529fdaf753830f3eadac62762329e07
george-potapov13 <bayjel13@gmail.com>
2019-12-13 05:56:17 +0200

    working with comments

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from django.views.generic import ListView
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from .models import Post, Comment
+from .models import Post
 from .forms import CommentForm
 from taggit.models import Tag
 from django.db.models import Q
@@ -42,30 +43,22 @@ def post_list(request, tag_slug=None):
 
 
 def post_detail(request, year, month, day, post):
-    # arguments year, month, day, post
-    # makes pusible take one post filter by url or date
     object_list = Post.objects.filter(status='Published')
     post = get_object_or_404(Post, slug=post,
                              status='Published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-
-    # list of active comments for current post
     comments = post.comments.filter(active=True)
     if request.method == 'POST':
-        # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
             new_comment.post = post
-            # Save the comment to the database
             new_comment.save()
+            return HttpResponseRedirect(request.path_info)
     else:
         comment_form = CommentForm()
-
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = object_list.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count(
